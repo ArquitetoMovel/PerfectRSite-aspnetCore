5e1d0d805146eb2ebbb37f33a7ddf097164ea134
Alexandre Danelon <alexandredanelon@MacBook-Pro-de-Alexandre.local>
2017-04-30 19:45:47 -0300

    POC do Artigo Asp.net vs PHP - Site Asp.net Core

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+
+namespace HugoSite_aspnetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        // private readonly ILogger _logger;
+        //   private readonly Stopwatch _stopWatch;
+
+        // public HomeController(ILogger<HomeController> logger){
+        //     _logger=logger;
+        //    _stopWatch = new Stopwatch();
+        // }
+
+        public  IActionResult Index()
+        {
+            // _stopWatch.Start();
+            return  View();
+        }
+
+        public IActionResult Services()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        // public override void OnActionExecuted(ActionExecutedContext filterContext)
+        // {
+        //     _stopWatch.Stop();
+        //     _logger.LogWarning($"Total do tempo decorrido {_stopWatch.ElapsedMilliseconds} ms", null);
+        
+        //     base.OnActionExecuted(filterContext);
+        //  // 
+        // }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace HugoSite_aspnetCore
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            // loggerFactory.AddDebug();
+            // loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            
+
+            // app.Use(async (context, next) =>
+            // {
+            //     var sw = new Stopwatch();
+            //     sw.Start();
+            //     await next.Invoke();
+            //     sw.Stop();
+            //     byte[] data = Encoding.UTF8.GetBytes($"<!-- {sw.ElapsedMilliseconds} ms -->");
+            //         context.Response.ContentType = "application/json";
+            //         await context.Response.Body.WriteAsync(data, 0, data.Length);
+            // });
+        }
+    }
+}
